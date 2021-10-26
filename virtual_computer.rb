3af76818380767c5db2c5bacccea90d50e12d43e
franciscohcf <franciscocavalcantefilho@gmail.com>
2021-10-26 13:18:31 -0300

    feat: computer class added

--- virtual_computer.rb
+++ virtual_computer.rb
@@ -1,3 +1,23 @@
 class Computer
-  def initialize; end
+  @@users = {}
+
+  def Computer.get_users
+    @@users
+  end
+
+  def initialize(username, password)
+    @username = username
+    @password = password
+    @files = {}
+    @@users[username] = password
+  end
+
+  def create(filename)
+    time = Time.now
+    @files[filename] = time
+    puts "#{filename} was created by #{@username} at #{time}."
+  end
 end
+
+my_computer = Computer.new("franciscohcf", "pass")
+
